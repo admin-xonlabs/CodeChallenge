7ee57c16363e49c1469f47d31dfa4f76a6f3d657
Ashok kumar K <52928917+admin-xonlabs@users.noreply.github.com>
2020-10-09 17:47:34 +0530

    CodeChallenge 1

---
+++ src/CodeChallenge1.java
@@ -0,0 +1,23 @@
+
+public class CodeChallenge1 {
+
+	public static void traverse(int[][] arr)
+	{
+		// your logic here
+		
+		
+	}
+
+	public static void main(String[] args) {
+
+		int[][] mat = {
+				{1,2,3,4,5},
+				{6,7,8,9,10},
+				{11,12,13,14,15},
+				{16,17,18,19,20},
+				{21,22,23,24,25}
+						};
+		
+	}
+
+}
