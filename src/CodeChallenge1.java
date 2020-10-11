1f96c3c55e58a919883a38b2a9d861c3bb48bb5b
SagarNGhalli <72400130+SagarNGhalli@users.noreply.github.com>
2020-10-11 15:15:31 +0530

    Update CodeChallenge1.java
    logic code done

--- src/CodeChallenge1.java
+++ src/CodeChallenge1.java
@@ -3,7 +3,172 @@ public class CodeChallenge1 {
 
 	public static void traverse(int[][] arr)
 	{
-		// your logic here
+	 int x,y;
+
+	    int len=5;
+
+	    for(int i=0;i<len;i++)
+
+	    {
+
+	        if(i%2==0)
+
+	        {
+
+	         x=i;
+
+	         y=0;
+
+	       
+
+	        while(x>=0 && y<=i)
+
+	        {
+
+	           System.out.println(arr[x][y]);
+
+	           x-=1;
+
+	           y+=1;
+	        }
+
+	        }
+
+	        else
+
+	        {
+
+	            x=0;
+
+	            y=i;
+
+	            while(x<=i && y>=0)
+
+	            {
+
+	                System.out.println(arr[x][y]);
+
+	                x+=1;
+
+	                y-=1;
+
+	            }
+
+	        }
+
+	    }
+
+
+
+   if(len%2==0)
+
+	        {
+
+	            for(int i=1;i<=len;i++)
+
+	            {
+
+	                if(i%2 !=0){
+
+	                x=len-1;
+
+	                y=i;
+
+	                while(x>=i && y<=len-1)
+
+	                {
+
+	                    System.out.println(arr[x][y]);
+
+	                    x--;
+
+	                    y++;
+
+	                }
+
+	                }
+
+	                else
+
+	                {
+
+	                    x=i;
+
+	                    y=len-1;
+
+	                    while(x<=len-1 && y>=i)
+
+	                    {
+
+	                       System.out.println(arr[x][y]);
+
+	                       x++;
+
+	                       y--;
+
+	                    }
+
+	                }
+
+	            }
+
+	            
+
+	        }
+
+	        else
+
+	        {
+
+	            for(int i=1;i<=len;i++)
+
+	            {
+
+	                if(i%2 !=0){
+
+	                x=i;
+
+	                y=len-1;
+
+	                while(x<=len-1 && y>=i)
+
+	                {
+
+	                    System.out.println(arr[x][y]);
+
+	                    x++;
+
+	                    y--;
+
+	                }
+
+	                }
+
+	                else
+
+	                {
+
+	                    x=len-1;
+
+	                    y=i;
+
+	                    while(x>=i && y<=len-1)
+
+	                    {
+
+	                       System.out.println(arr[x][y]);
+
+	                       x--;
+
+	                       y++;
+
+	                    }
+
+	                }
+
+	            }
+
+	        }
 		
 		
 	}
