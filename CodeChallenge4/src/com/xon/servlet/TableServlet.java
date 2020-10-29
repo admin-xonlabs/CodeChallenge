01a58d99e7b96dad84321855569d8a3828768e87
vinayaka16041999 <vinayaka16041999@gmail.com>
2020-10-29 17:06:17 +0530

    Changed the jsp for name tag and java for displaying the table

--- CodeChallenge4/src/com/xon/servlet/TableServlet.java
+++ CodeChallenge4/src/com/xon/servlet/TableServlet.java
@@ -17,7 +17,36 @@ public class TableServlet extends HttpServlet
       resp.setContentType("text/html");
       PrintWriter pw = resp.getWriter();
       pw.print("DYNAMIC TABLE MUST BE GENERATED HERE");
+      int m=Integer.parseInt(req.getParameter("row"));
+      int n=Integer.parseInt(req.getParameter("column"));
+      String [][] a=new String[m+1][n+1];
+      String b="#F6F6F6";
+      if(m>0 && n>0) {
+    	for(int i=1;i<m+1;i++) {
+    		for(int j=1;j<n+1;j++) {
+    			String str="Row:"+i+",Coln:"+j;
+    			a[i][j]=str;
+    		}
+    	}
+    	pw.print("<table style='border:2px solid black'>");
+    	for(int i=1;i<m+1;i++) {
+    		if(b=="#F6F6F6") {
+    			b="#ded9d9";
+    		}
+    		else {
+    			b="#F6F6F6";
+    		}
+    		pw.print("<tr style='background-color:"+b+"'>");
+    		for(int j=1;j<n+1;j++) {
+    			pw.print("<td style='border:2px solid black'>"+a[i][j]+"</td>");
+    		}
+    		pw.print("</tr>");
+    	}
+    	pw.print("</table>");
+      }
+      else {
+    	pw.print("Enter a valid row and column value");
+      }
       pw.close();
    }
-
-}
+}   
