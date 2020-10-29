7b8b6e5fc2bb5f524356d0ed680b991b8d0d792b
AKANKSHAGOWRI <71918758+AKANKSHAGOWRI@users.noreply.github.com>
2020-10-29 15:11:36 +0530

    Update TableServlet.java

--- CodeChallenge4/src/com/xon/servlet/TableServlet.java
+++ CodeChallenge4/src/com/xon/servlet/TableServlet.java
@@ -16,7 +16,42 @@ public class TableServlet extends HttpServlet
    {            
       resp.setContentType("text/html");
       PrintWriter pw = resp.getWriter();
-      pw.print("DYNAMIC TABLE MUST BE GENERATED HERE");
+      try{
+      int m = Integer.parseInt(req.getParameter("rows"));
+      int n = Integer.parseInt(req.getParameter("coloumns"));
+      String[][] a = new String[r+1][c+1];
+      String b = "";
+      if(m!=0 || n!=0){
+      for(int i = 1; i < r+1; i++)
+      {
+    	  for(int j = 1; j < c+1; j++)
+    	  {
+    		  String str = "Row " + i + ", Col " +j;
+    		  a[i][j] = str;
+    	  }
+      }
+      pw.print("<table style='border:2px solid black'>");
+
+      for(int i = 1; i < r+1; i++)
+      {
+    	  if(b == "silver")
+	   {
+		   return "yellow";
+	   }
+	   else
+	   {
+		   return "silver";
+	   }
+    	  pw.print("<tr style='background-color:" + u + "'>");
+    	  for(int j = 1; j < c+1; j++)
+    	  {
+    		  pw.print("<td style='border:1px solid black'>" + a[i][j] +"</td>");
+    	  }
+    	  pw.print("</tr>");
+      }
+      pw.print("</table>");
+      }
+      }
       pw.close();
    }
 
